ec3b59c978754c720cb23ee9540df7e19ad2d227
Emil Stjerneman <emil@stjerneman.com>
2010-03-01 00:05:34 +0000

    Added by anon: Added scrollbars to the popup

--- editors/linkit_tinymce/linkit_tinymce.js
+++ editors/linkit_tinymce/linkit_tinymce.js
@@ -1,15 +1,28 @@
 // $Id$
 
+Linkit = {
+  openDialog: function(dialog, params) {
+    var editor = tinyMCE.get(tinyMCE.activeEditor.editorId);
+    editor.windowManager.open({
+      file: dialog.url + '/' + this.field,
+      width: dialog.width,
+      height: dialog.height,
+      inline: 1,
+      scrollbars: dialog.scrollbars
+    }, params);
+  }
+};
+
 Drupal.wysiwyg.plugins["linkit_tinymce"] = {    
   /**
    * Execute the button.
    */
   invoke: function(data, settings, instanceId) {  	
   	// Options to pass to the dialog.
-		var options = { id: instanceId, content: data.content };
+		var options = { id: instanceId, content: data.content, inline : true, scrollbars : true };
     // Open dialogue.
-    
-    Drupal.wysiwyg.instances[instanceId].openDialog(settings.dialog, options);
+    console.log(settings);
+    Linkit.openDialog(settings.dialog, options);
   },
 
   attach: function(content, settings, instanceId) {
