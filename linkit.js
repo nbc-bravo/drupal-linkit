4b34782c3a6648dbd83070d068f40061d091d082
Emil Stjerneman <emil@stjerneman.com>
2010-08-02 16:02:54 +0000

    #860832 Added by anon: Integrate Linkit with IMCE

--- linkit.js
+++ linkit.js
@@ -31,5 +31,19 @@ var linkit_helper = {
   show_no_selection_text : function() {
     var info_text = Drupal.t('<em class="notice">Notice: No selection element was found, your link text will appear as the item title you are linking to.</em>');
     $('#edit-link-wrapper').prepend(info_text);    
+  },
+  /*
+   * IMCE integration
+   */
+  openFileBrowser : function () {
+    window.open(Drupal.settings.basePath + 'imce?app=Linkit|url@edit-link', '', 'width=760,height=560,resizable=1');
   }
 }
+
+Drupal.behaviors.linkit_imce = function(context) {
+  $('#linkit-imce').click(function() {
+    linkit_helper.openFileBrowser();
+    return false;
+  });
+}
+
