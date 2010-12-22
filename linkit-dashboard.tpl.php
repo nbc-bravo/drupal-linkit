c96ad90aba8ee779042cf4ba390cd2d4404cea10
Emil Stjerneman <emil@stjerneman.com>
2010-12-22 20:28:02 +0000

    First commit for D7 release

---
+++ linkit-dashboard.tpl.php
@@ -0,0 +1,12 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Linkit dashboard template, controls the output in the popup
+ */
+?>
+<div id="linkit">
+  <h1><?php print t('Insert internal or external link'); ?></h1>
+  <?php print render($form); ?>
+</div>
