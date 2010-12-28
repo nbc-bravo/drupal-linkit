a53e4899af03d7c55c936abb3306560a115190fa
Emil Stjerneman <emil@stjerneman.com>
2010-12-28 20:46:12 +0000

    by anon: Fixed the popup dashboard and changed some menu callback urls so they   dont piont to admin/linkit

--- linkit-dashboard.tpl.php
+++ linkit-dashboard.tpl.php
@@ -1,12 +1,25 @@
 <?php
 // $Id$
-
-/**
- * @file
- * Linkit dashboard template, controls the output in the popup
- */
 ?>
-<div id="linkit">
-  <h1><?php print t('Insert internal or external link'); ?></h1>
-  <?php print render($form); ?>
-</div>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyled Content in IE */ ?> </script>
+</head>
+<body>
+  <div id="linkit">
+    <?php if (!empty($messages)): print $messages; endif; ?>
+    <?php if (!empty($help)): print $help; endif; ?>
+    <div id="content-content" class="clear-block">
+      <?php print $form; ?>
+    </div>
+  </div>
+  <?php print $closure; ?>
+</body>
+</html>
