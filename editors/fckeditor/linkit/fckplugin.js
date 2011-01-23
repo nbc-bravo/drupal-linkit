c0cb48a4f9013b6e12163c46d8fbbdc30f6d2204
Emil Stjerneman <emil@stjerneman.com>
2011-01-23 23:24:30 +0000

    #1030936 by johannesdr, anon: Fixed/Added more reliable path strings for   FCKeditor and CKeditor.

--- editors/fckeditor/linkit/fckplugin.js
+++ editors/fckeditor/linkit/fckplugin.js
@@ -1,16 +1,11 @@
 /* $Id$ */
 
+var path = Drupal.settings.linkit.url.wysiwyg_fckeditor;
+var basePath =  Drupal.settings.basePath;
+var modulePath = Drupal.settings.linkit.modulepath;
 
-  if(typeof Drupal != 'undefined') {
-    var path = Drupal.settings.linkit.url.wysiwyg_fckeditor;
-    var basePath =  Drupal.settings.basePath;
-  } else {
-    var basePath =  linkit_basePath;
-    var path = basePath + linkit_url_fckeditor;
-  }
+FCKCommands.RegisterCommand( 'linkit', new FCKDialogCommand( 'linkit', '&nbsp;', path, 580, 320 ) ) ;
 
-  FCKCommands.RegisterCommand( 'linkit', new FCKDialogCommand( 'linkit', '&nbsp;', path, 580, 320 ) ) ;
-
-  var oLinkitItem = new FCKToolbarButton( 'linkit', 'Linkit');
-  oLinkitItem.IconPath = basePath + 'sites/all/modules/linkit/editors/fckeditor/linkit/linkit.png';
-  FCKToolbarItems.RegisterItem( 'linkit', oLinkitItem );
+var oLinkitItem = new FCKToolbarButton( 'linkit', 'Linkit');
+oLinkitItem.IconPath = basePath + modulePath + '/editors/fckeditor/linkit/linkit.png';
+FCKToolbarItems.RegisterItem( 'linkit', oLinkitItem );
