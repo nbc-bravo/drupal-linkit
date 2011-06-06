a6f031617e3222abcb08ab3ebe0c067bca7cea32
Emil Stjerneman <emil@stjerneman.com>
2011-06-06 19:38:37 +0200

    by anon: Fixed TinyMCE integration.

--- editors/tinymce/linkit.js
+++ editors/tinymce/linkit.js
@@ -4,65 +4,53 @@
 var linkitDialog = {};
 
 (function ($) {
+
 linkitDialog = {
-	init : function() {
-  	var ed = tinyMCEPopup.editor;
-  	// Setup browse button
-  	if (e = ed.dom.getParent(ed.selection.getNode(), 'A')) {
-  	
-  	  // Delete the anchor from the URL, this will be added later on anyway
-  	  var href = $(e).attr('href');
-  	
-  	  if(href.length > 0) {
-  		  // linkitHelper.search_styled_link(href);
-  			} 
-  	  $('#edit-title').val($(e).attr('title'));
-  	  return false;
-  	}
-  	// TODO: Change all linkitHelper references to
-  	// Drupal.linkit.<new_function_name>
-  	// linkitHelper.populateTitle(ed.selection.getContent());
-  	Drupal.linkit.populateLink(ed.selection.getContent(), '', true);
-	},
-  
+  init : function() {
+    var ed = tinyMCEPopup.editor;
+
+    if (e = ed.dom.getParent(ed.selection.getNode(), 'A')) {
+      $('#edit-title').val($(e).attr('title'));
+      $('#edit-id').val($(e).attr('id'));
+      $('#edit-class').val($(e).attr('class'));
+      $('#edit-rel').val($(e).attr('rel'));
+      $('#edit-accesskey').val($(e).attr('accesskey'));
+      console.log(e);
+      Drupal.linkit.populateLink($(e).html(), $(e).attr('href'), true);
+    }
+    else {
+      // TODO: Change all linkitHelper references to
+      // Drupal.linkit.<new_function_name>
+      Drupal.linkit.populateLink(ed.selection.getContent(), $(e).attr('href'), true);
+    }
+  },
+
   insertLink : function() {
     var ed = tinyMCEPopup.editor, e;
 
     tinyMCEPopup.restoreSelection();
-		e = ed.dom.getParent(ed.selection.getNode(), 'A');
-    
-    // Remove element if there is no href
-		if ($('#edit-link--2').val() == "") {
-			if (e) {
-				tinyMCEPopup.execCommand("mceBeginUndoLevel");
-				ed.dom.remove(e, 1);
-				tinyMCEPopup.execCommand("mceEndUndoLevel");
-				tinyMCEPopup.close();
-				return;
-			}
+    e = ed.dom.getParent(ed.selection.getNode(), 'A');
+
+    if ($('#edit-path').val() == "") {
+      // Remove element if there is no href
+      if (e) {
+        tinyMCEPopup.execCommand("mceBeginUndoLevel");
+        ed.dom.remove(e, 1);
+        tinyMCEPopup.execCommand("mceEndUndoLevel");
+        tinyMCEPopup.close();
+        return;
+      }
       tinyMCEPopup.close();
-		  return;
-		}
+      return;
+    }
     
     tinyMCEPopup.execCommand("mceBeginUndoLevel");
-		
-    var matches = $('#edit-link--2').val().match(/\[path:(.*)\]/i);
-    href = (matches == null) ? $('#edit-link--2').val() : matches[1];
-    
-    // Add anchor if we have any and make sure there is no "#" before adding the anchor
-    // But do not add if there is an anchor in the URL
-    var anchor = $('#edit-anchor').val().replace(/#/g,'');
-    var hasAnchor = $('#edit-link--2').val().match(/\#/i);
-    
-    if(anchor.length > 0 && hasAnchor == null ) {
-      href = href.concat('#' + anchor);
-    }
 
-    var link_text_matches = $('#edit-link--2').val().match(/(.*)\[path:.*\]/i);
-    link_text = (link_text_matches == null) ? $('#edit-link--2').val() : link_text_matches[1].replace(/^\s+|\s+$/g, '');
+    var link_path = $('#edit-path').val();
+    var link_text = $('#edit-text').val();
 
     // Create new anchor elements
-		if (e == null) {
+    if (e == null) {
       
       if (ed.selection.isCollapsed()) {
         tinyMCEPopup.execCommand("mceInsertContent", false, '<a href="#linkit-href#">' + link_text + '</a>');
@@ -70,30 +58,30 @@ linkitDialog = {
         tinyMCEPopup.execCommand("createlink", false, '#linkit-href#', {skip_undo : 1});
       }
         
-			tinymce.each(ed.dom.select("a"), function(n) {
-				if (ed.dom.getAttrib(n, 'href') == '#linkit-href#') {
-					e = n;
+      tinymce.each(ed.dom.select("a"), function(n) {
+        if (ed.dom.getAttrib(n, 'href') == '#linkit-href#') {
+          e = n;
 
-					ed.dom.setAttribs(e, {
-						'href'      : href,
+          ed.dom.setAttribs(e, {
+            'href'      : link_path,
             'title'     : $('#edit-title').val(),
             'id'        : $('#edit-id').val(),
             'class'     : $('#edit-class').val(),
             'rel'       : $('#edit-rel').val(),
             'accesskey' : $('#edit-accesskey').val()
-					});
-				}
-			});
-		} else {
-			ed.dom.setAttribs(e, {
-				'href'      : href,
+          });
+        }
+      });
+    } else {
+      ed.dom.setAttribs(e, {
+        'href'      : link_path,
         'title'     : $('#edit-title').val(),
         'id'        : $('#edit-id').val(),
         'class'     : $('#edit-class').val(),
         'rel'       : $('#edit-rel').val(),
         'accesskey' : $('#edit-accesskey').val()
-			});
-		}
+      });
+    }
     // Don't move caret if selection was image
     if(e != null) {
       if (e.childNodes.length != 1 || e.firstChild.nodeName != 'IMG') {
@@ -104,8 +92,8 @@ linkitDialog = {
       }
     }
 
-		tinyMCEPopup.execCommand("mceEndUndoLevel");
-		tinyMCEPopup.close();
+    tinyMCEPopup.execCommand("mceEndUndoLevel");
+    tinyMCEPopup.close();
 
   }
 };
