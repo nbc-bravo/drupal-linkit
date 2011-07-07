599278d43267d508773da5459f33e215cdfd67ad
Emil Stjerneman <emil@stjerneman.com>
2011-07-08 00:10:57 +0200

    by anon: Impoved the API document.

--- linkit.api.php
+++ linkit.api.php
@@ -15,13 +15,12 @@
  * http://api.drupal.org/api/drupal/developer--topics--forms_api_reference.html
  *
  * @param string $profile
- *   The profile the user is about to use with this attributes.
+ *   The profile settings the user calling this function has.
  * @return string
- *   An associative array with form elements defining your attributes.
+ *   An associative array with form elements with the key being the HTML 
+ *   attribute name (my_attribute = <a my_attribute="value"></a>).
  */
 function hook_linkit_attributes($profile) {
-  // The array key here will be the attribute that is inserted.
-  // I.E <a my_attribute="value"></a>.
   $attributes['my_attribute'] = array(
     '#type' => 'textfield',
     '#title' => t('My attribute'),
@@ -61,23 +60,69 @@ function hook_linkit_attributes_alter(&$attributes) {
  * @return
  *   An associative array with the key being the machine name for the
  *   implementation and the values being an array with the following keys:
- *     - "title": The human readable name for the plugin.
- *     - "description": Short description for the plugin.
- *     - "file": (optional) The file where the "autocomplete callback" function 
- *       defined in.
- *     - "autocomplete callback": The function to call when the users search for
- *       something.
+ *     - "title": The untranslated human readable name for the plugin.
+ *     - "description": Short untranslated description for the plugin.
+ *     - "file": (optional) A file that will be included before the 
+ *       autocomplete callback function is called.
+ *     - "autocomplete callback": The function to call when the users search
+ *       for something.
  *     - "path info callback": (optional) @TODO
  */
 function hook_linkit_plugins() {
   $plugins['myplugin'] = array(
-    'title' => t('My plugin'),
-    'description' => t('My plugin implementation'),
+    'title' => 'My plugin',
+    'description' => 'My plugin implementation',
     'file' => drupal_get_path('module', 'mymodule') . '/mymodule.inc',
-    'autocomplete callback' => 'mymodule_autocomplete',
-    'path info callback' => 'mymodule_path_info'
+    'autocomplete callback' => 'mymodule_autocomplete_function',
+    'path info callback' => 'mymodule_path_info_function'
   );
   return $plugins;
 }
 
-// @TODO: Define how the 'autocomplete callback' function should work.
+/**
+ * "autocomplete callback" given in hook_linkit_plugins().
+ *
+ * This function is called when the user is typing in the autocomplete field
+ * and the search is triggerd.
+ *
+ * @param $string
+ *   A string contains the text from the autocomplete field.
+ * @param $profile
+ *   The profile settings the user calling this function has.
+ * @return
+ *   An array of search results. Each search result is an associative array 
+ *   that may contain the following key-value pairs:
+ *     - "title": The untranslated title for the item.
+ *     - "description": (optional) The untranslated description that will be
+ *       shown under the title.
+ *     - "path": The path for the item.
+ *     - "group": (optional) The untranslated group name. This is used to group
+ *       results into different groups, like Content, user, term and so on.
+ */ 
+function mymodule_autocomplete_function($string, $profile) {
+}
+
+/**
+ * "path info callback" given in hook_linkit_plugins().
+ *
+ * Retrieve relevant information about a URL.
+ *
+ * Note: Do not return en empty result, return FALSE instead as the first
+ * returned value will be used in this call stack.
+ *
+ * @param $path_info
+ *   An associative array containing information about the URL requested.
+ * @param $profile
+ *   The profile settings the user calling this function has.
+ * @return
+ *   An array of search results. Each search result is an associative array 
+ *   that may contain the following key-value pairs:
+ *     - "title": The untranslated title for the item.
+ *     - "description": (optional) The untranslated description that will be
+ *       shown under the title.
+ *     - "path": The path for the item.
+ *     - "group": (optional) The untranslated group name. This is used to group
+ *       results into different groups, like Content, user, term and so on.
+ */ 
+function mymodule_path_info_function($path_info, $profile) {
+}
