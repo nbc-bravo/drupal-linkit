0e46e4bbab7112723d76ff097f1663c8723cca7b
Emil Stjerneman <emil@stjerneman.com>
2011-09-16 23:48:25 +0200

    Cleanup

--- plugins/export_ui/linkit_profiles_ui.class.php
+++ plugins/export_ui/linkit_profiles_ui.class.php
@@ -9,50 +9,32 @@
  * Base class for export UI.
  */
 class linkit_profiles_ui extends ctools_export_ui {
-  /**
-   * Provide the table header.
-   *
-   * If you've added columns via list_build_row() but are still using a
-   * table, override this method to set up the table header.
-   */
+
   function list_table_header() {
     $header = array();
-    if (!empty($this->plugin['export']['admin_title'])) {
-      $header[] = array('data' => t('Title'), 'class' => array('ctools-export-ui-title'));
-    }
-
+    $header[] = array('data' => t('Title'), 'class' => array('ctools-export-ui-title'));
     $header[] = array('data' => t('Name'), 'class' => array('ctools-export-ui-name'));
     $header[] = array('data' => t('Storage'), 'class' => array('ctools-export-ui-storage'));
     $header[] = array('data' => t('Roles'), 'class' => array('ctools-export-ui-roles'));
     $header[] = array('data' => t('Weight'), 'class' => array('ctools-export-ui-weight'));
     $header[] = array('data' => t('Operations'), 'class' => array('ctools-export-ui-operations'));
-
     return $header;
   }
 
-  /**
-   * Build a row based on the item.
-   *
-   * By default all of the rows are placed into a table by the render
-   * method, so this is building up a row suitable for theme('table').
-   * This doesn't have to be true if you override both.
-   */
   function list_build_row($item, &$form_state, $operations) {
     // Set up sorting
-    $name = $item->{$this->plugin['export']['key']};
+    $name = $item->name;
     $schema = ctools_export_get_schema($this->plugin['schema']);
 
-    // Note: $item->{$schema['export']['export type string']} should have already been set up by export.inc so
-    // we can use it safely.
     switch ($form_state['values']['order']) {
       case 'disabled':
         $this->sorts[$name] = $item->weight;
         break;
       case 'disabled_title':
-        $this->sorts[$name] = empty($item->disabled) . $name;
+        $this->sorts[$name] = empty($item->disabled) . $item->admin_title;
         break;
       case 'title':
-        $this->sorts[$name] = $item->{$this->plugin['export']['admin_title']};
+        $this->sorts[$name] = $item->admin_title;
         break;
       case 'name':
         $this->sorts[$name] = $name;
@@ -62,29 +44,20 @@ class linkit_profiles_ui extends ctools_export_ui {
         break;
     }
 
-    $this->rows[$name]['data'] = array();
-    $this->rows[$name]['class'] = !empty($item->disabled) ? array('ctools-export-ui-disabled') : array('ctools-export-ui-enabled');
-
-    // If we have an admin title, make it the first row.
-    if (!empty($this->plugin['export']['admin_title'])) {
-      $this->rows[$name]['data'][] = array('data' => check_plain($item->{$this->plugin['export']['admin_title']}), 'class' => array('ctools-export-ui-title'));
-    }
-    $this->rows[$name]['data'][] = array('data' => check_plain($name), 'class' => array('ctools-export-ui-name'));
-    $this->rows[$name]['data'][] = array('data' => check_plain($item->{$schema['export']['export type string']}), 'class' => array('ctools-export-ui-storage'));
-
-    $role_lost = $this->buildRoleList($item->role_rids);
-    $this->rows[$name]['data'][] = array('data' => check_plain($role_lost), 'class' => array('ctools-export-ui-roles'));
-
-    $this->rows[$name]['data'][] = array('data' => check_plain($item->weight), 'class' => array('ctools-export-ui-weight'));
-
     $ops = theme('links__ctools_dropbutton', array('links' => $operations, 'attributes' => array('class' => array('links', 'inline'))));
+    $role_lost = $this->buildRoleList($item->role_rids);
 
-    $this->rows[$name]['data'][] = array('data' => $ops, 'class' => array('ctools-export-ui-operations'));
-
-    // Add an automatic mouseover of the description if one exists.
-    if (!empty($this->plugin['export']['admin_description'])) {
-      $this->rows[$name]['title'] = $item->{$this->plugin['export']['admin_description']};
-    }
+    $this->rows[$name] = array(
+      'data' => array(
+        array('data' => check_plain($item->admin_title), 'class' => array('ctools-export-ui-title')),
+        array('data' => check_plain($name), 'class' => array('ctools-export-ui-name')),
+        array('data' => check_plain($item->{$schema['export']['export type string']}), 'class' => array('ctools-export-ui-storage')),
+        array('data' => check_plain($role_lost), 'class' => array('ctools-export-ui-roles')),
+        array('data' => check_plain($item->weight), 'class' => array('ctools-export-ui-weight')),
+        array('data' => $ops, 'class' => array('ctools-export-ui-operations')),
+      ),
+      'class' => array(!empty($item->disabled) ? 'ctools-export-ui-disabled' : 'ctools-export-ui-enabled'),
+    );
   }
 
   /**
@@ -99,31 +72,14 @@ class linkit_profiles_ui extends ctools_export_ui {
     return implode(', ', $list);
   }
 
-  /**
-   * Provide a list of sort options.
-   *
-   * Override this if you wish to provide more or change how these work.
-   * The actual handling of the sorting will happen in build_row().
-   */
   function list_sort_options() {
-    $options = array('disabled' => t('Weight'));
-    if (!empty($this->plugin['export']['admin_title'])) {
-      $options += array(
-        'disabled_title' => t('Enabled, title'),
-        $this->plugin['export']['admin_title'] => t('Title'),
-      );
-    }
-    else {
-      $options += array(
-        'disabled_title' => t('Enabled, name'),
-      );
-    }
-
-    $options += array(
+    $options = array(
+      'disabled' => t('Weight'),
+      'disabled_title' => t('Enabled, title'),
+      'admin_title'=> t('Title'),
       'name' => t('Name'),
       'storage' => t('Storage'),
     );
-
     return $options;
   }
 }
