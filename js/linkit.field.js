2aa495089d295be698bf1960b848f621d3ddd159
Emil Stjerneman <emil@stjerneman.com>
2012-03-05 08:28:43 +0100

    Link fields work almost.

---
+++ js/linkit.field.js
@@ -0,0 +1,59 @@
+/**
+ * @file
+ * Linkit field ui functions
+ */
+
+// Create the linkit namespaces.
+Drupal.linkit = Drupal.linkit || {};
+Drupal.linkit.field = Drupal.linkit.field || {};
+
+(function ($) {
+
+Drupal.behaviors.linkit_field = {
+  attach: function(context, settings) {
+    // If there is no fields, just stop here.
+    if (settings.linkit.fields == null) {
+      return false;
+    }
+    console.log(settings.linkit.fields);
+    $.each(settings.linkit.fields, function(field) {
+      console.log(field);
+      // Create a "Better Autocomplete" object, see betterautocomplete.js
+      $('#' + field, context).betterAutocomplete('init',
+        settings.linkit.autocompletePath,
+        settings.linkit.autocomplete,
+        { // Callbacks
+        select: function(result) {
+          // Only change the link text if it is empty
+          if (typeof result.disabled != 'undefined' && result.disabled) {
+            return false;
+          }
+          $('#' + field, context).val(function(index, value) {
+            return result.path;
+          });
+        },
+        constructURL: function(path, search) {
+          return path + encodeURIComponent(search);
+        },
+        insertSuggestionList: function($results, $input) {
+          var position = $input.position();
+          $results.width($input.innerWidth())
+            .css({
+              position: 'absolute',
+              left: parseInt(position.left, 10),
+              top: position.top + $input.outerHeight(true),
+
+              zIndex: 2000,
+              maxHeight: '330px',
+              // Visually indicate that results are in the topmost layer
+             boxShadow: '0 0 15px rgba(0, 0, 0, 0.5)'
+            })
+            .hide()
+            .insertAfter($input);
+          }
+      });
+    });
+  }
+};
+
+})(jQuery);
