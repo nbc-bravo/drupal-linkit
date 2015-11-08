0dc98f691d6867a09bfb03e762e892334c680ab1
Emil Stjerneman <emil@stjerneman.com>
2015-11-08 01:27:03 +0100

    Moved overview test into profile crud test

--- src/Tests/ProfileCrudTest.php
+++ src/Tests/ProfileCrudTest.php
@@ -16,12 +16,53 @@ use Drupal\Component\Utility\Unicode;
 class ProfileCrudTest extends LinkitTestBase {
 
   /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['block'];
+
+  /**
    * {@inheritdoc}
    */
   protected function setUp() {
     parent::setUp();
 
     $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Test the overview page.
+   */
+  function testOverview() {
+    $this->drupalPlaceBlock('local_actions_block');
+    $this->drupalPlaceBlock('local_tasks_block');
+
+    // Verify that the profile collection page is not accessible for regular
+    // users.
+    $this->drupalLogin($this->baseUser);
+    $this->drupalGet('admin/config/content/linkit');
+    $this->assertResponse(403);
+    $this->drupalLogout();
+
+    // Verify that the profile collection page is accessible for regular users.
+    $this->drupalLogin($this->adminUser);
+
+    $profiles = [];
+    $profiles[] = $this->createProfile();
+    $profiles[] = $this->createProfile();
+
+    $this->drupalGet('admin/config/content/linkit');
+    $this->assertResponse(200);
+
+    // Assert that the 'Add profile' action exists.
+    $this->assertLinkByHref('admin/config/content/linkit/add');
+
+    /** @var \Drupal\linkit\ProfileInterface $profile */
+    foreach ($profiles as $profile) {
+      $this->assertLinkByHref('admin/config/content/linkit/manage/' . $profile->id());
+      $this->assertLinkByHref('admin/config/content/linkit/manage/' . $profile->id() . '/delete');
+    }
   }
 
   /**
@@ -37,7 +78,7 @@ class ProfileCrudTest extends LinkitTestBase {
     $edit['description'] = $this->randomMachineName(16);
     $this->drupalPostForm('admin/config/content/linkit/add', $edit, t('Save and manage matchers'));
 
-    $this->assertRaw(t('Created new profile %label.', array('%label' => $edit['label'])));
+    $this->assertRaw(t('Created new profile %label.', ['%label' => $edit['label']]));
     $this->drupalGet('admin/config/content/linkit');
     $this->assertText($edit['label'], 'Profile exists in the profile collection.');
   }
@@ -60,7 +101,7 @@ class ProfileCrudTest extends LinkitTestBase {
     $edit['description'] = $this->randomMachineName(16);
     $this->drupalPostForm('admin/config/content/linkit/manage/' . $profile->id(), $edit, t('Update profile'));
 
-    $this->assertRaw(t('Updated profile %label.', array('%label' => $edit['label'])));
+    $this->assertRaw(t('Updated profile %label.', ['%label' => $edit['label']]));
     $this->drupalGet('admin/config/content/linkit');
     $this->assertText($edit['label'], 'Updated profile exists in the profile collection.');
   }
@@ -74,8 +115,9 @@ class ProfileCrudTest extends LinkitTestBase {
 
     $this->drupalPostForm('admin/config/content/linkit/manage/' . $profile->id() . '/delete', [], t('Delete'));
 
-    $this->assertRaw(t('The linkit profile %label has been deleted.', array('%label' => $profile->label())));
+    $this->assertRaw(t('The linkit profile %label has been deleted.', ['%label' => $profile->label()]));
     $this->drupalGet('admin/config/content/linkit');
     $this->assertNoText($profile->label(), 'Deleted profile does not exists in the profile collection.');
   }
+
 }
