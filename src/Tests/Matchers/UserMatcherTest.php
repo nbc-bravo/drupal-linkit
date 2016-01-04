4a2234a38b51f8f8a2c0e0bd03a7ba30a8c1ba5a
Emil Stjerneman <emil@stjerneman.com>
2016-01-04 01:36:05 +0100

    Added role filter to user matcher

--- src/Tests/Matchers/UserMatcherTest.php
+++ src/Tests/Matchers/UserMatcherTest.php
@@ -42,8 +42,12 @@ class UserMatcherTest extends LinkitTestBase {
     $custom_role_admin = $this->drupalCreateRole(array(), 'custom_role_admin', 'custom_role_admin');
 
     $this->drupalCreateUser([], 'lorem');
-    $this->drupalCreateUser([], 'ipsumlorem');
     $this->drupalCreateUser([], 'foo');
+
+    $account = $this->drupalCreateUser([], 'ipsumlorem');
+    $account->addRole($custom_role);
+    $account->save();
+
     $account = $this->drupalCreateUser([], 'lorem_custom_role');
     $account->addRole($custom_role);
     $account->save();
@@ -68,9 +72,26 @@ class UserMatcherTest extends LinkitTestBase {
   }
 
   /**
+   * Tests user matcher with role filer.
+   */
+  function testUserMatcherWidthRoleFiler() {
+    /** @var \Drupal\linkit\MatcherInterface $plugin */
+    $plugin = $this->manager->createInstance('entity:user', [
+      'settings' => [
+        'roles' => [
+          'custom_role' => 'custom_role'
+        ],
+      ],
+    ]);
+
+    $matches = $plugin->getMatches('Lorem');
+    $this->assertEqual(2, count($matches), 'Correct number of matches');
+  }
+
+  /**
    * Tests user matcher with include blocked setting activated.
    */
-  function testUserMatcherWidthIncludeBlocked() {
+  function testUserMatcherWidthIncludeBlockedWithPermissions() {
     /** @var \Drupal\linkit\MatcherInterface $plugin */
     $plugin = $this->manager->createInstance('entity:user', [
       'settings' => [
@@ -78,8 +99,15 @@ class UserMatcherTest extends LinkitTestBase {
       ],
     ]);
 
-    $matches = $plugin->getMatches('Lorem');
-    $this->assertEqual(5, count($matches), 'Correct number of matches');
-  }
+    // Test without permissions to see blocked users.
+    $matches = $plugin->getMatches('blocked');
+    $this->assertEqual(0, count($matches), 'Correct number of matches');
 
+    $account = $this->drupalCreateUser(['administer users']);
+    $this->drupalLogin($account);
+
+    // Test with permissions to see blocked users.
+    $matches = $plugin->getMatches('blocked');
+    $this->assertEqual(1, count($matches), 'Correct number of matches');
+  }
 }
