7de68aec44fbbbfdb1b166c0852e435e97c44a79
Emil Stjerneman <emil@stjerneman.com>
2016-01-05 06:03:04 +0100

    Added image show dimensions settings to file matcher.

--- src/Plugin/Linkit/Matcher/FileMatcher.php
+++ src/Plugin/Linkit/Matcher/FileMatcher.php
@@ -6,6 +6,8 @@
  */
 
 namespace Drupal\linkit\Plugin\Linkit\Matcher;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\linkit\Utility\LinkitXss;
 
 /**
  * @Matcher(
@@ -20,10 +22,64 @@ class FileMatcher extends EntityMatcher {
   /**
    * {@inheritdoc}
    */
+  public function getSummary() {
+    $summery = parent::getSummary();
+
+    $summery[] = $this->t('Show image dimensions: @show_image_dimensions', [
+      '@show_image_dimensions' => $this->configuration['images']['show_dimensions'] ? $this->t('Yes') : $this->t('No'),
+    ]);
+
+    return $summery;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+      'images' => [
+        'show_dimensions' => FALSE,
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function calculateDependencies() {
     return parent::calculateDependencies() + [
       'module' => ['file'],
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $form['images'] = array(
+      '#type' => 'details',
+      '#title' => t('Image file settings'),
+      '#description' => t('Extra settings for image files in the result.'),
+      '#tree' => TRUE,
+    );
+
+    $form['images']['show_dimensions'] = [
+      '#title' => t('Show pixel dimensions'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['images']['show_dimensions'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+    $this->configuration['images'] = $form_state->getValue('images');
   }
 
   /**
@@ -34,6 +90,28 @@ class FileMatcher extends EntityMatcher {
     $query->condition('status', FILE_STATUS_PERMANENT);
 
     return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function buildDescription($entity) {
+    $description_array = array();
+
+    $description_array[] = parent::buildDescription($entity);
+
+    /** @var \Drupal\file\FileInterface $entity */
+    $file = $entity->getFileUri();
+
+    /** @var \Drupal\Core\Image\ImageInterface $image */
+    $image = \Drupal::service('image.factory')->get($file);
+    if ($image->isValid() && $this->configuration['images']['show_dimensions']) {
+        $description_array[] = $image->getWidth() . 'x' . $image->getHeight() . 'px';
+    }
+
+    $description = implode('<br />' , $description_array);
+
+    return LinkitXss::descriptionFilter($description);
   }
 
 }
