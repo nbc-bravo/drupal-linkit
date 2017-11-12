a0c02deb6913bc53e79ab36f2c84bb69b902e912
Zombienaute <Zombienaute@1036766.no-reply.drupal.org>
2017-11-12 02:29:58 +0100

    Issue #2915618 by acbramley: Fix coding standards so tests run

--- src/Plugin/Linkit/Matcher/EntityMatcher.php
+++ src/Plugin/Linkit/Matcher/EntityMatcher.php
@@ -415,7 +415,7 @@ class EntityMatcher extends ConfigurableMatcherBase {
    *   The matched entity.
    *
    * @return string
-   *    The metadata for this entity.
+   *   The metadata for this entity.
    */
   protected function buildDescription(EntityInterface $entity) {
     $description = \Drupal::token()->replace($this->configuration['metadata'], [$this->targetType => $entity], ['clear' => TRUE]);
